d688a667300a949fad55a7a979c529ea099a014e
AxayacatlS <38516115+AxayacatlS@users.noreply.github.com>
2021-06-17 19:48:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,12 +10,13 @@
 </head>
 
 <body style='margin: 0; overflow: hidden;'>
+	<!-- 17.346390324130684, -98.03114595356641-->
     <a-scene
         vr-mode-ui="enabled: false"
 		embedded
         arjs='sourceType: webcam; sourceWidth:1280; sourceHeight:960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false;'>
 
-        <a-entity gps-entity-place="latitude: 44.4908615; longitude: 11.3326416;" gltf-model="./assets/magnemite/scene.gltf" rotation="0 180 0" animation-mixer scale="0.15 0.15 0.15"/>
+        <a-entity gps-entity-place="latitude: 17.346390324130684; longitude: -98.03114595356641;" gltf-model="./assets/magnemite/scene.gltf" rotation="0 180 0" animation-mixer scale="0.15 0.15 0.15"/>
         <a-camera gps-camera rotation-reader></a-camera>
 	</a-scene>
 </body>
